7c2a4e2b7875353718508155f17c83878c4eec69
YashuAmaresh <yashaswini.amaresh@gmail.com>
2015-07-28 10:45:50 +0530

    Update TransactionTable.java
    Added checkRelation method and displayTable method

--- Transaction/TransactionTable.java
+++ Transaction/TransactionTable.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+
 
 public class TransactionTable {
 	ArrayList<TransactionRow> trTable= new ArrayList<TransactionRow>();
@@ -23,16 +23,41 @@ public class TransactionTable {
 	
 	void insertInOutputTable(TransactionRow tr)
 	{
-		for(int i=0 ;i<transactionTable.size()-1;i++)
+		for(int i=0 ;i<trTable.size()-1;i++)
 		{
-			for(int j=1;j<transactionTable.size();j++){
-				checkRelation(transactionTable.get(i),transactionTable.get(j),i,j);
+			for(int j= i + 1;j > 0 ;j--){
+				checkRelation(trTable.get(i), trTable.get(j), i, j);
 			}
 		}
 	}
 	
-	void checkRelation(TransactionRow row1,TransactionRow row2,)
+	void checkRelation(TransactionRow row1, TransactionRow row2, int i, int j)
 	{
+		if (row1.statusCode == row2.statusCode && row1.transferCode == row2.transferCode) {
+			if (!row1.rangeObj.isDisjoint(row2.rangeObj)) {
+				row1.mergeRows(row2);
+			}
+		}
+		
+		if (row1.rangeObj.classify(row2.rangeObj) == Range.Relation.SAME) {
+			row1.statusCode = row2.statusCode;
+			row1.transferCode = row2.transferCode;
+		}
+		
+		if (row1.statusCode != row2.statusCode || row1.transferCode != row2.transferCode) {
+			if (!row1.rangeObj.isDisjoint(row2.rangeObj)) {
+				TransactionRow newRow = row1.splitRow(row2);
+				trTable.add(i + 1, row2);
+				trTable.add(i + 2, newRow);				
+			}
+			
+		}
+	}
+	
+	void displayTable() {
+		for (TransactionRow row : trTable) {
+			System.out.println(row.rangeObj.lo + " " + row.rangeObj.hi + " " + row.statusCode + " " + row.transferCode);
+		}
 	}
 	
 }
